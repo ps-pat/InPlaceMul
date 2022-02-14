f308427fba7b0381ad2fa87ec56b4e8f9d0e0028
Patrick Fournier <p_fournier@hushmail.com>
2022-02-14 18:44:39 -0500

    Performance improvement.

--- src/InplaceMul.jl
+++ src/InplaceMul.jl
@@ -4,13 +4,22 @@ export mulB!
 
 using LinearAlgebra: lmul!
 
+"""
+    mulB!(A, B)
+
+For a square matrix `A`, compute `A * B` quasi in place, storing the result
+in `B`.
+"""
 function mulB!(A::Matrix, B::Matrix)
-    @inbounds for colB ∈ range(1, stop = size(B, 2))
+    nrowA = size(A, 1)
+
+    @inbounds for colB ∈ range(1, length = size(B, 2))
         tmp = B[:,colB]
         B[:,colB] .= zero(eltype(B))
 
-        @inbounds @simd for colA ∈ range(1, stop = size(A, 2))
-            B[:,colB] .+= tmp[colA] * @view(A[:,colA])
+        @inbounds @simd for k ∈ eachindex(A)
+            B[(k - 1) % nrowA + 1, colB] +=
+                tmp[(k - 1) ÷ nrowA + 1] * A[k]
         end
     end
 
